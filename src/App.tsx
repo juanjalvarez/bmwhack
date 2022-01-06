924d006c3fdced5d7bdc99cdd4d8ea616054795b
Juan J. Alvarez <jalvarez5241997@gmail.com>
2022-01-06 01:20:06 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { CarItemObject, carList } from "./dataConverter";
+import ReactDataGrid from "@inovua/reactdatagrid-community";
+import "@inovua/reactdatagrid-community/index.css";
+
+const usdFormatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+  maximumFractionDigits: 0,
+});
+
+const PHOTO_HEIGHT = 100;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="ag-theme-alpine"
+      style={{ width: window.innerWidth, height: window.innerHeight }}
+    >
+      <ReactDataGrid
+        style={{ width: window.innerWidth, height: window.innerHeight }}
+        rowHeight={PHOTO_HEIGHT}
+        dataSource={carList.filter((car) => {
+          return (
+            [
+              car.bodyStyle === "Coupe",
+              car.orderStatus === "1",
+              Boolean(car.imgURL),
+            ].indexOf(false) === -1
+          );
+        })}
+        defaultGroupBy={["bodyStyle"]}
+        columns={[
+          {
+            name: "modelYear",
+            header: <span>Year</span>,
+          },
+          {
+            name: "bodyStyle",
+            header: <span>Body</span>,
+          },
+          {
+            name: "price",
+            header: <span>Price</span>,
+            type: "number",
+            render: (a: { data: CarItemObject }) => {
+              return (
+                <h2>
+                  <b>{usdFormatter.format(a.data.price)}</b>
+                </h2>
+              );
+            },
+          },
+          {
+            name: "location",
+            header: <span>Location</span>,
+          },
+          {
+            name: "link",
+            header: <span>Link</span>,
+            render: (a: { data: CarItemObject }) => {
+              if (!a.data.link) {
+                return <span>No URL</span>;
+              }
+              return (
+                <a href={a.data.link} target="_blank" rel="noreferrer">
+                  link
+                </a>
+              );
+            },
+          },
+          {
+            name: "imgURL",
+            header: <span>Photo</span>,
+            flex: 1,
+            render: (a: { data: CarItemObject }) => {
+              return (
+                <img
+                  alt={a.data.vin}
+                  src={a.data.imgURL}
+                  style={{
+                    height: PHOTO_HEIGHT,
+                  }}
+                />
+              );
+            },
+          },
+        ]}
+      />
     </div>
   );
 }
---
+++ src/dataConverter.ts
@@ -0,0 +1,40 @@
+import data from './data.json'
+
+
+const dealerMap: Record<string, typeof data.data.getInventory.dealerInfo[0]> = {}
+
+const fillDealerMap = () => {
+    data.data.getInventory.dealerInfo.forEach(dealer => {
+        dealerMap[dealer.centerID] = dealer
+    })
+}
+
+fillDealerMap()
+
+export type CarItemObject = {
+    modelYear: number
+    bodyStyle: string
+    price: number
+    vin: string
+    link?: string
+    imgURL: string
+    orderStatus: string
+    location: string
+}
+
+const generateData = () => data.data.getInventory.result.map<CarItemObject>(item => {
+    const dealer = dealerMap[item.dealerId]
+    const address = dealer.newVehicleSales[0].address[0]
+    return {
+        modelYear: item.modelYear,
+        bodyStyle: item.bodyStyle.name,
+        price: item.totalMsrp,
+        vin: item.vin,
+        link: item.vehicleDetailsPage,
+        imgURL: item.initialCOSYURL,
+        orderStatus: item.orderStatus,
+        location: `${address.city}, ${address.state}`
+    }
+})
+
+export const carList = generateData()
